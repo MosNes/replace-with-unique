ee5390b2ad63bfc5e5ff939ac4812700164be7db
MosNes <7hekarl@gmail.com>
2023-11-17 10:25:31 -0500

    added logic to replace strings, tested

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 import fs from 'fs';
 import path from 'path';
+import { URL } from 'url';
 
+//necessary for __dirname and __filename to work with ES6 modules
+const __filename = new URL('', import.meta.url).pathname;
+const __dirname = new URL('.', import.meta.url).pathname;
+
+//path to file
 const filePath = 'C:/Users/moses.nester/Desktop/Steven Creed - mailFilters - Text.txt';
 
-let file = fs.readFileSync(path.resolve(__dirname, filePath), 'utf8');
+//string to search for
+const searchString = '~!~';
 
-console.log(file);
+//string to replace with
+const replaceString = 'Floob';
+
+let fileText = fs.readFileSync(path.resolve(__dirname, filePath), 'utf8');
+
+console.log(`Searching document for instances of ${searchString}...`);
+
+let position = fileText.search(searchString);
+
+let increment = 1;
+
+while( position !== -1 ) {
+    fileText = fileText.replace(searchString, increment.toString());
+    position = fileText.search(searchString);
+    increment++;
+}
+
+console.log(`Finished searching document. Replaced ${increment-1} instances of ${searchString}.`);
